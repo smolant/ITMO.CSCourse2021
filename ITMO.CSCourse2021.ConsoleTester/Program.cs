8de0317b921a988fb90be32867f8fce413b72424
Anton Smolianinov <smolant@hotmail.com>
2021-04-27 20:10:03 +0300

    Add project files.

---
+++ ITMO.CSCourse2021.ConsoleTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstVal = 5, secondVal = 7;
+            Console.WriteLine(firstVal + secondVal);
+
+            Console.WriteLine("Hello, ITMO");
+
+
+            string s = Console.ReadLine();
+
+            int x = int.Parse(s);
+        }
+    }
+}
